fd8cfdb91ea50e9059e70acf73c80e5fb6e1ec05
daniela.SantaCruz <daniela.santacruz@koerber-supplychain.com>
2026-02-21 19:28:30 -0400

    Updated eslint config file

--- .history/frontend/eslint.config_20260221192754.js
+++ .history/frontend/eslint.config_20260221192754.js
@@ -3,10 +3,10 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
-
+ 
 export default defineConfig([
   globalIgnores(["dist"]),
-
+ 
   // Config principal React
   {
     files: ["**/*.{js,jsx}"],
@@ -39,3 +39,5 @@ export default defineConfig([
     },
   },
 ]);
+ 
+ 
